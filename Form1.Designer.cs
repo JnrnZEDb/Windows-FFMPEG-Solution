97f36cc3eeedf78c1c83ab702e80e41eb99f228e
Blake DeCarvalho <blakedecarvalho@gmail.com>
2018-09-03 20:06:19 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Windows_FFMPEG_Solution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoUploadButton = new System.Windows.Forms.Button();
+            this.video1TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // videoUploadButton
+            // 
+            this.videoUploadButton.Location = new System.Drawing.Point(707, 202);
+            this.videoUploadButton.Name = "videoUploadButton";
+            this.videoUploadButton.Size = new System.Drawing.Size(345, 92);
+            this.videoUploadButton.TabIndex = 0;
+            this.videoUploadButton.Text = "Upload a Video";
+            this.videoUploadButton.UseVisualStyleBackColor = true;
+            this.videoUploadButton.Click += new System.EventHandler(this.videoUploadButton_Click);
+            // 
+            // video1TextBox
+            // 
+            this.video1TextBox.Location = new System.Drawing.Point(53, 224);
+            this.video1TextBox.Multiline = true;
+            this.video1TextBox.Name = "video1TextBox";
+            this.video1TextBox.Size = new System.Drawing.Size(536, 70);
+            this.video1TextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1310, 622);
+            this.Controls.Add(this.video1TextBox);
+            this.Controls.Add(this.videoUploadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button videoUploadButton;
+        private System.Windows.Forms.TextBox video1TextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_FFMPEG_Solution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void videoUploadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openvideo1FileDialog = new OpenFileDialog();
+            openvideo1FileDialog.Filter = "Choose a video file|*.mp4";
+            openvideo1FileDialog.Title = "Select a Cursor File";
+            if (openvideo1FileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.  
+                video1TextBox.Text = openvideo1FileDialog.InitialDirectory + openvideo1FileDialog.FileName; ;
+            }
+        }
+    }
+}
